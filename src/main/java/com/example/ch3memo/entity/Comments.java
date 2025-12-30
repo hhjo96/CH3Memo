96d0b60f6fdb535ff6c982f3a57319a980226fd2
hhjo <43205471+hhjo96@users.noreply.github.com>
2025-12-30 17:35:49 +0900

    ADD: 엔티티 comments, FIX: memo의 nullable 조건

--- src/main/java/com/example/ch3memo/entity/Comments.java
+++ src/main/java/com/example/ch3memo/entity/Comments.java
@@ -9,29 +9,25 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name = "memos")
+@Table(name = "comments")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Memo extends BaseEntity{
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class Comments extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank @Size(max = 30) @Column(length = 100, nullable = false) // 한글 30글자가 db length 30보다 클 수 있다고 하여 수정
-    private String title;
+    @Column(nullable = false)
+    private Long memoId;
     @Size(max = 200) @Column(length = 700)
     private String body;
     @Column(nullable = false)
     private Long userId;
+    @Column
     private String password;
 
-    public Memo(String title, String body, Long userId, String password) {
-        this.title = title;
+    public Comments(Long memoId, String body, Long userId, String password) {
+        this.memoId = memoId;
         this.body = body;
         this.userId = userId;
         this.password = password;
-    }
-
-    public void update(String title, Long userId) {
-        this.title = title;
-        this.userId = userId;
     }
 }
