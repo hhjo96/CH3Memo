216da86bfc4ae94f8481cbcc5ee15f40a612d833
hhjo <43205471+hhjo96@users.noreply.github.com>
2025-12-31 11:21:21 +0900

    FIX: @Valid를 컨트롤러에 붙여야 한다고 하여 수정함

--- src/main/java/com/example/ch3memo/controller/CommentController.java
+++ src/main/java/com/example/ch3memo/controller/CommentController.java
@@ -5,6 +5,7 @@ import com.example.ch3memo.dto.CommentCreateResponse;
 
 import com.example.ch3memo.dto.MemoCommentGetResponse;
 import com.example.ch3memo.service.CommentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +17,9 @@ public class CommentController {
 
     private final CommentService commentService;
 
-    //c
+    //create
     @PostMapping("/memos/{memoId}/comments")
-    public ResponseEntity<CommentCreateResponse> create(@PathVariable Long memoId, @RequestBody CommentCreateRequest request){
+    public ResponseEntity<CommentCreateResponse> create(@Valid @PathVariable Long memoId, @RequestBody CommentCreateRequest request){
         return ResponseEntity.status(HttpStatus.CREATED).body(commentService.save(memoId, request));
     }
 
